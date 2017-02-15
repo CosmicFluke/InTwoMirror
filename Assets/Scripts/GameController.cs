905be4203188bbe78280b48a070c6c588f0bfbf4
stephentactual <stephen.dennis@tactuallabs.com>
2017-02-14 23:15:18 -0500

    adding initial game controller and scenes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    static GameController Instance;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Instance == null)
+        {
+            GameObject.DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha0))
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("Level1");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("Level2");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("Level3");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene("ClosingScene");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha9))
+        {
+            SceneManager.LoadScene("OpeningScene");
+        }
+
+    }
+}

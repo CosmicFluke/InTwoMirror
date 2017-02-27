5d14260db4e82d85be359e5afbcb8b6a4664a440
Mohammad Abubakr <mabubakr94@gmail.com>
2017-02-26 20:39:25 -0500

    Added trigger to remove fog

---
+++ Assets/Scripts/FogTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogTrigger : MonoBehaviour {
+
+    public ParticleSystem fog;
+    bool destroyFog = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fog != null && destroyFog)
+        {
+            ParticleSystem.Particle[] particles = new ParticleSystem.Particle[fog.particleCount];
+            fog.GetParticles(particles);
+
+            fog.startColor = new Color(fog.startColor.r, fog.startColor.g, fog.startColor.b, fog.startColor.a * 0.9f);
+
+            if (fog.startColor.a < 0.1)
+            {
+                Destroy(fog, 5);
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        destroyFog = true;
+    }
+}

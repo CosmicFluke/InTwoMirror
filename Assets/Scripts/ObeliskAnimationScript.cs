678d2a6b8c72e96f14254eb4b00040920663d94c
Abed Fayyad <AbedFayyad@live.com>
2017-02-28 13:36:12 -0500

    Add Obelisk prefab and related resources

---
+++ Assets/Scripts/ObeliskAnimationScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ObeliskAnimationScript : MonoBehaviour {
+
+    public float RotationRate = 20f;
+    public float BouncingRate = 0.1f;
+    public bool ShouldRotate = true;
+    public bool ShouldBounce = true;
+
+    private float initialTipHeight;
+
+	// Use this for initialization
+	void Start () {
+        initialTipHeight = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ShouldRotate) transform.Rotate(new Vector3(0, 0, Time.deltaTime * RotationRate));
+        if (ShouldBounce) transform.position = new Vector3(transform.position.x, initialTipHeight + (Mathf.Sin(Time.time) * BouncingRate), transform.position.z);
+    }
+}

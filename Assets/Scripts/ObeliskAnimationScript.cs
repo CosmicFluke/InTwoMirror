df476287bb2233d7858e64fa88f9dcf5f169399d
Abed Fayyad <AbedFayyad@live.com>
2017-02-28 13:42:17 -0500

    Change spaces to tabs

--- Assets/Scripts/ObeliskAnimationScript.cs
+++ Assets/Scripts/ObeliskAnimationScript.cs
@@ -2,21 +2,21 @@
 
 public class ObeliskAnimationScript : MonoBehaviour {
 
-    public float RotationRate = 20f;
-    public float BouncingRate = 0.1f;
-    public bool ShouldRotate = true;
-    public bool ShouldBounce = true;
+	public float RotationRate = 20f;
+	public float BouncingRate = 0.1f;
+	public bool ShouldRotate = true;
+	public bool ShouldBounce = true;
 
-    private float initialHeight;
+	private float initialHeight;
 
 	// Use this for initialization
 	void Start () {
-        initialHeight = transform.position.y;
+		initialHeight = transform.position.y;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        if (ShouldRotate) transform.Rotate(new Vector3(0, 0, Time.deltaTime * RotationRate));
-        if (ShouldBounce) transform.position = new Vector3(transform.position.x, initialHeight + (Mathf.Sin(Time.time) * BouncingRate), transform.position.z);
-    }
+		if (ShouldRotate) transform.Rotate(new Vector3(0, 0, Time.deltaTime * RotationRate));
+		if (ShouldBounce) transform.position = new Vector3(transform.position.x, initialHeight + (Mathf.Sin(Time.time) * BouncingRate), transform.position.z);
+	}
 }

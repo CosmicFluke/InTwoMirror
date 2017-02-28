3ea03747d49a124ca97331016d74bdc66bc69fde
Abed Fayyad <AbedFayyad@live.com>
2017-02-28 13:39:25 -0500

    Rename variable

--- Assets/Scripts/ObeliskAnimationScript.cs
+++ Assets/Scripts/ObeliskAnimationScript.cs
@@ -7,16 +7,16 @@ public class ObeliskAnimationScript : MonoBehaviour {
     public bool ShouldRotate = true;
     public bool ShouldBounce = true;
 
-    private float initialTipHeight;
+    private float initialHeight;
 
 	// Use this for initialization
 	void Start () {
-        initialTipHeight = transform.position.y;
+        initialHeight = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (ShouldRotate) transform.Rotate(new Vector3(0, 0, Time.deltaTime * RotationRate));
-        if (ShouldBounce) transform.position = new Vector3(transform.position.x, initialTipHeight + (Mathf.Sin(Time.time) * BouncingRate), transform.position.z);
+        if (ShouldBounce) transform.position = new Vector3(transform.position.x, initialHeight + (Mathf.Sin(Time.time) * BouncingRate), transform.position.z);
     }
 }

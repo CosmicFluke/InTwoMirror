13b68ecb7e51c97509dc073edba4265a5751051a
Asher Minden-Webb <asher.minden.webb@mail.utoronto.ca>
2017-03-01 02:37:09 -0500

    Big update.  Fixed musical algorithm bug.  Added fade-in/fade-out to musical tones.  Added music to scenes.  Added key mapping for starting/stopping music. Mapped Start button to advance to next level. Balanced sound levels for resonators.

--- Assets/Scripts/ResonatorController.cs
+++ Assets/Scripts/ResonatorController.cs
@@ -11,8 +11,10 @@ public class ResonatorController : MonoBehaviour {
     public Material activeMaterial;
     public float glowIntensity = 7f;
     public PlayerID player;
+    public float fadeFactor = 0.5f;
 
     bool isPlaying = false;
+    bool isStopping = false;
     GameObject playerObj;
     Key targetKey;
     const int baseNote = 57;
@@ -31,38 +33,67 @@ public class ResonatorController : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
         if (isPlaying && oscillatingFrequency > 0)
         {
             Hv_ObeliskVoice_v1_AudioLib audio = GetComponent<Hv_ObeliskVoice_v1_AudioLib>();
             audio.SetFloatParameter(Hv_ObeliskVoice_v1_AudioLib.Parameter.Mastervoicevolume, 0.6f * Mathf.Abs(Mathf.Sin(ossCounter * oscillatingFrequency * Mathf.PI / 60)) + 0.25f);
             ossCounter++;
         } else ossCounter = 0;
+        AudioSource sound = GetComponent<AudioSource>();
+        if (isPlaying && sound.volume < 1) {
+            sound.volume = sound.volume + fadeFactor * Time.deltaTime;
+            Light glow = GetComponentInChildren<Light>();
+        if (isPlaying && glow.intensity < glowIntensity)
+            glow.intensity = glow.intensity + (fadeFactor * glowIntensity) * Time.deltaTime;
+        }
+
+        if (isStopping)
+        {
+            fadeOut();
+        }
 		
 	}
 
     void start() {
         if (isPlaying) return;
         AudioSource sound = GetComponent<AudioSource>();
-        sound.Play();
+        if (isStopping)
+            isStopping = false;
+        else
+        {
+            sound.volume = 0;
+            sound.Play();
+        }
         isPlaying = true;
         MeshRenderer mrend = GetComponentInChildren<MeshRenderer>();
         mrend.material = activeMaterial;
         mrend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+    }
+
+    void fadeOut()
+    {
+        AudioSource sound = GetComponent<AudioSource>();
+        if (sound.volume > 0)
+            sound.volume = sound.volume - fadeFactor * Time.deltaTime;
         Light glow = GetComponentInChildren<Light>();
-        glow.intensity = glowIntensity;
+        if (glow.intensity > 0)
+            glow.intensity = glow.intensity - (fadeFactor * glowIntensity) * Time.deltaTime;
+
+        if (glow.intensity <= 0 && sound.volume <= 0)
+        {
+            isStopping = false;
+            sound.Stop();
+        }
     }
 
     void stop() {
         if (!isPlaying) return;
-        AudioSource sound = GetComponent<AudioSource>();
-        sound.Stop();
+        isStopping = true;
         isPlaying = false;
         MeshRenderer mrend = GetComponentInChildren<MeshRenderer>();
         mrend.material = baseMaterial;
         mrend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-        Light glow = GetComponentInChildren<Light>();
-        glow.intensity = 0;
     }
 
     public void activate(GameObject activatingPlayer, int tone) {
@@ -96,8 +127,14 @@ public class ResonatorController : MonoBehaviour {
     bool checkTone(int tone) {
         int[] offsets = HarmonyUtil.Offsets[targetKey.Chord];
         int tonic = (int) targetKey.Tone;
-
-        foreach (int offset in offsets) if (Mathf.Abs((tone - baseNote) % 12 - tonic) == offset) return true;
+        int note = (tone - baseNote) % 12;
+        
+        int offset = (note - tonic + 12) % 12;
+        Debug.Log("Checking: " + ((Tone) note).ToString() + " is " + offset.ToString() + " above " + ((Tone) tonic).ToString());
+        foreach (int scaleOffset in offsets)
+        {
+            if (scaleOffset == offset) return true;
+        }
         return false;
     }
 }

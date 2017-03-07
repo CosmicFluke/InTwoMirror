44e97f200a41f144ee7f5d033b7824a81c66ef56
Asher Minden-Webb <asher.minden.webb@mail.utoronto.ca>
2017-03-07 15:17:16 -0500

    Assets and test scripts added for pivot.  Nothing fully implemented yet.

---
+++ Assets/Scripts/RegionController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RegionState { A, B, C }
+public enum RegionEffect { Stable, Unstable, Volatile }
+public enum Actions { Shift, Flip,  }
+
+public class RegionController : MonoBehaviour {
+
+    public static RegionEffect StateToEffect(RegionState state, int offset)
+    {
+        return (RegionEffect)(((int)state + offset) % 3);
+    }
+
+    public GameObject[] hexTiles;
+    public GameObject[] neighbouringRegions;
+
+    public RegionState State { get { return this.currentState; } }
+
+    private RegionState currentState;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShiftState(int offset) {
+        currentState = (RegionState)(((int)currentState + offset) % 3);
+    }
+}

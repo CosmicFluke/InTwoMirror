da2ecdc40392f15163c3e4c3116e8ca548ff53d6
Abed Fayyad <AbedFayyad@live.com>
2017-03-08 22:08:25 -0500

    Add character prefabs including animations

---
+++ Assets/Scripts/GirlPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlPlayer : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("up")) {
+			animator.SetInteger ("AnimPar", 1);
+		} else if (Input.GetKey ("q")) {
+			animator.SetInteger ("AnimPar", 2);
+		} else {
+			animator.SetInteger("AnimPar", 0);
+		}
+	}
+}

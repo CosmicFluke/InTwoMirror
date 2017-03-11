75ec5a90903f250b2cdf4eb540c22c3e65b721c9
Senisa <senisa.s@gmail.com>
2017-03-11 15:38:03 -0800

    Added dynamically instantiated health UI to LevelController

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
@@ -10,24 +11,65 @@ public class LevelController : MonoBehaviour
     private float _levelCompletion = 0;
 
     private bool _musicPlaying = true;
-    private const bool Debug = true;
+    private const bool levelDebug = true;
 
     // The amount of actions each player starts with, could be unique for each level.
     public int StartActionOne = 3;
     public int StartActionTwo = 3;
 
+    public GameObject canvas;
+    private static Text P1Health;
+    private static Text P2Health;
+
     // Use this for initialization
     void Start()
     {
-        if (_gameController == null)
-        {
-            _gameController = GameObject.Find("GameController").GetComponent<GameController>();
-        }
+        //if (_gameController == null)
+        //{
+        //    _gameController = GameObject.Find("GameController").GetComponent<GameController>();
+        //}
 
         _levelCompletion = 0;
 
+        if (canvas == null && FindObjectOfType(typeof(Canvas)) == null)
+        {
+            canvas = (GameObject) Instantiate(Resources.Load("UI/BlankCanvas"));
+            Debug.Log("Canvas instantiated");
+        }
+        else if (canvas == null)
+        {
+            canvas = (GameObject) FindObjectOfType(typeof(Canvas));
+            Debug.Log("canvas linked");
+        }
+
+        // Programatically instantiate player health UI
+        P1Health = ((GameObject) Instantiate(Resources.Load("UI/PlayerHealth"))).GetComponent<Text>();
+        P1Health.transform.SetParent(canvas.transform);
+        P2Health = ((GameObject)Instantiate(Resources.Load("UI/PlayerHealth"))).GetComponent<Text>();
+        P2Health.transform.SetParent(canvas.transform);
+
+        // TODO: finalize clean positioning
+        Vector3 bottomLeft = new Vector3(100, 10, 0);
+        Vector3 bottomRight = new Vector3(canvas.GetComponent<RectTransform>().rect.width - 50, 10, 0);
+
+        P1Health.GetComponent<RectTransform>().position = bottomLeft;
+        P2Health.GetComponent<RectTransform>().position = bottomRight;
+
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject player in players)
+        {
+            Debug.Log(player.name);
+            UpdatePlayerHealth(player.GetComponent<CharacterControl>().player, player.GetComponent<CharacterControl>().HealthPoints);
+        }
     }
 
+    public static void UpdatePlayerHealth(PlayerID id, int hp)
+    {
+        if(id == PlayerID.P1)
+            P1Health.text = "Player 1: " + hp;
+        else if(id == PlayerID.P2)
+            P2Health.text = "Player 2: " + hp;
+    }
     void Update()
     {
         // On level complete
@@ -37,7 +79,7 @@ public class LevelController : MonoBehaviour
 				_gameController.GotoScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
-        if (Debug)
+        if (levelDebug)
         {
             if (Input.GetButtonDown("Start"))
             {

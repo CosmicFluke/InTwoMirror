cd2b4d630f14b695affb88ca437b72195cc1238c
Mohammad Abubakr <mabubakr94@gmail.com>
2017-03-11 16:04:01 -0500

    Added simple script for playing a clip and starting the particle effect.

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+	bool isPlaying = false;
+	bool isStopping = false;
+
+	public AudioSource sound;
+
+	public AudioClip test;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (sound.isPlaying) {
+			GetComponent<ParticleSystem> ().Play ();
+		} else {
+			stopSound ();
+		}
+	}
+
+	void startSound (AudioClip clip)
+	{
+		sound.clip = clip;
+		sound.Play ();
+
+		notifyInteractive ();
+	}
+
+	void stopSound ()
+	{
+		GetComponent<ParticleSystem> ().Stop ();
+	}
+
+	void notifyInteractive ()
+	{
+		// TODO: Add in interaction with regions.
+	}
+		
+}

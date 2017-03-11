66a397ecf763ca036c55a2aeac3b25775360a3de
Asher Minden-Webb <asher.minden.webb@mail.utoronto.ca>
2017-03-11 15:29:25 -0500

    Renamed CharacterControl script and added new PlayerActionController script.  More cleanup.

---
+++ Assets/Scripts/HexTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTile : MonoBehaviour {
+
+    // Alternate to HexMesh, for use with pre-made Hex model
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerActionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActionController : MonoBehaviour {
+
+    public Region currentRegion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
